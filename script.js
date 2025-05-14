b865a43ebaec09dfa39313ae43700b29ca881944
Celmo4k <patrickfranco2000@gmail.com>
2025-05-14 11:55:41 +0100

    new new new new

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log("What's good nigga");
-function subtraction() {
-    int num1 = 10;
-    int num2 = 2;
+function subtraction(num1, num2) {
+    num1 = 10;
+    num2 = 2;
 
     return num1 - num2;
 };
