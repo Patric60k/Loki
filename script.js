12d6b7d6cab2c77d2d0badf8780d6a2e0b6776ee
mockingjae <132662396+mockingjae@users.noreply.github.com>
2025-05-14 12:01:56 +0100

    new C

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 console.log("What's good nigga");
-<<<<<<< HEAD
+
 function subtraction(num1, num2) {
     num1 = 10;
     num2 = 2;
 
     return num1 - num2;
 };
-=======
+
 
 function somar(i,j ){
     return i + j;
@@ -15,4 +15,4 @@ function somar(i,j ){
 function write(){
     console.log("If your are reading this, your gay");
 }
->>>>>>> 98ea6a8f6e05a9a4232c8e4d983a6d3c8e2ce1a7
+
